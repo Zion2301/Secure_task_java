a91256927980e65c01dc73a354bacc7264ccc76b
Zion Omogbeme <omogbemezion@gmail.com>
2025-05-07 11:22:06 +0100

    added new files

--- src/main/java/com/aptech/Secure/Tasker/dtos/LoginRequest.java
+++ src/main/java/com/aptech/Secure/Tasker/dtos/LoginRequest.java
@@ -1,4 +1,21 @@
 package com.aptech.Secure.Tasker.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class LoginRequest {
+    @Email(message = "Must be a valid email address")
+    @NotBlank(message = "Email must not be blank")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
 }
