b4f5a5898ddc38da230f5aac7cbde09aaf018a7f
Zion Omogbeme <omogbemezion@gmail.com>
2025-05-07 11:40:42 +0100

    done wit b morning class

--- src/main/java/com/aptech/Secure/Tasker/dtos/responses/LoginResponse.java
+++ src/main/java/com/aptech/Secure/Tasker/dtos/responses/LoginResponse.java
@@ -1,5 +1,20 @@
 package com.aptech.Secure.Tasker.dtos.responses;
 
-public class LoginResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-}
+import java.time.Instant;
+
+public record LoginResponse (
+        @JsonProperty("access_token")
+         String accessToken,
+
+                @JsonProperty("token_type")
+ String tokenType,
+
+@JsonProperty("expires_in")
+ long expiresIn,
+
+@JsonProperty("expires_at")
+ Instant expiresAt
+) {}
